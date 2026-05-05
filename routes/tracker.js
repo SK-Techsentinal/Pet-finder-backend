dfab834f2a1c0ae1aa8bc22cf01faa7c30b6c2d6
Suleiman <skaldine2@gmail.com>
2026-05-05 15:26:41 +0200

    Update tracker.js

--- routes/tracker.js
+++ routes/tracker.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const authMiddleware = require('../middleware/auth');
+const authenticate = require('../middleware/authenticate');
 const nodemailer = require('nodemailer');
 
 // GET tracker status for a pet
